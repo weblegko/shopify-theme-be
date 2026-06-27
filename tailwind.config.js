8d61814f8bf245a7b144871adea1da8e076d3a07
ecomsys <webpolka@gmail.com>
2026-06-28 02:45:42 +0300

    add new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,9 @@ export default {
   theme: {
     screens: {
       xs: "576px",
-      sm: "640px",
+      sm: "640px",      
       md: "768px",
+      lg990: "990px",
       lg: "1024px",
       xl: "1280px",
       "2xl": "1536px",
